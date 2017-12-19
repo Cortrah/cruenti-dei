9afb11b8cc0e35870ca3fd98fa10767410c5e6e9
Cortlandt Winters <cort@cortwinters.net>
2017-12-19 08:05:44 -0500

    mix the two layouts

--- src/layouts/FullScreenFlex.vue
+++ src/layouts/FullScreenFlex.vue
@@ -1,64 +1,58 @@
 <template>
-    <div>
-        <el-row>
-            <el-col :span="24">
-                <el-menu
-                        :default-active="activeIndex2"
-                        class="el-menu-demo"
-                        mode="horizontal"
-                        @select="handleSelect"
-                        background-color="#333"
-                        text-color="#fff"
-                        active-text-color="#ffd04b">
-                    <el-menu-item index="1">OLD-IMPERIAL</el-menu-item>
-                    <el-submenu index="2">
-                        <template slot="title">Map</template>
-                        <el-menu-item index="2-1">Sahul</el-menu-item>
-                        <el-menu-item index="2-2">The Underlands</el-menu-item>
-                        <el-menu-item index="2-3">Oratoa</el-menu-item>
-                        <el-menu-item index="2-4">Effan</el-menu-item>
-                    </el-submenu>
-                    <el-submenu index="3">
-                        <template slot="title">Turn 23: 1445-1449</template>
-                        <el-menu-item index="3-1">1445</el-menu-item>
-                        <el-menu-item index="3-2">1446</el-menu-item>
-                        <el-menu-item index="3-3">1447</el-menu-item>
-                        <el-menu-item index="3-4">1448</el-menu-item>
-                        <el-menu-item index="3-5">1449</el-menu-item>
-                    </el-submenu>
+    <el-container>
+        <el-header>
+                    <el-menu
+                            :default-active="activeIndex2"
+                            class="el-menu-demo"
+                            mode="horizontal"
+                            @select="handleSelect"
+                            background-color="#333"
+                            text-color="#fff"
+                            active-text-color="#ffd04b">
+                        <el-menu-item index="1">OLD-IMPERIAL</el-menu-item>
+                        <el-submenu index="2">
+                            <template slot="title">Map</template>
+                            <el-menu-item index="2-1">Sahul</el-menu-item>
+                            <el-menu-item index="2-2">The Underlands</el-menu-item>
+                            <el-menu-item index="2-3">Oratoa</el-menu-item>
+                            <el-menu-item index="2-4">Effan</el-menu-item>
+                        </el-submenu>
+                        <el-submenu index="3">
+                            <template slot="title">Turn 23: 1445-1449</template>
+                            <el-menu-item index="3-1">1445</el-menu-item>
+                            <el-menu-item index="3-2">1446</el-menu-item>
+                            <el-menu-item index="3-3">1447</el-menu-item>
+                            <el-menu-item index="3-4">1448</el-menu-item>
+                            <el-menu-item index="3-5">1449</el-menu-item>
+                        </el-submenu>
 
-                    <el-menu-item index="4">
-                        <a href="http://www.sardarthion.com" target="_blank">
-                            Cruenti Dei
-                        </a>
-                    </el-menu-item>
-                </el-menu>
-            </el-col>
-        </el-row>
-
-        <el-row>
-            <el-col :span="12" class="game-map">
-                <game-map/>
-            </el-col>
-            <el-col :span="12">
-                <el-tabs type="border-card">
-                    <el-tab-pane label="TURN">
-                        <two-column-layout/>
-                    </el-tab-pane>
-                    <el-tab-pane label="REALM">REALM</el-tab-pane>
-                    <el-tab-pane label="LEADER">LEADER</el-tab-pane>
-                    <el-tab-pane label="RULES">RULES</el-tab-pane>
-                    <el-tab-pane label="DIPLOMACY">DIPLOMACY</el-tab-pane>
-                </el-tabs>
-            </el-col>
-        </el-row>
-
-        <el-row>
-            <el-col :span="24">
-                Footer
-            </el-col>
-        </el-row>
-    </div>
+                        <el-menu-item index="4">
+                            <a href="http://www.sardarthion.com" target="_blank">
+                                Cruenti Dei
+                            </a>
+                        </el-menu-item>
+                    </el-menu>
+        </el-header>
+        <el-main>
+            <el-row>
+                <el-col :span="12" class="game-map">
+                    <game-map/>
+                </el-col>
+                <el-col :span="12">
+                    <el-tabs type="border-card">
+                        <el-tab-pane label="TURN">
+                            <two-column-layout/>
+                        </el-tab-pane>
+                        <el-tab-pane label="REALM">REALM</el-tab-pane>
+                        <el-tab-pane label="LEADER">LEADER</el-tab-pane>
+                        <el-tab-pane label="RULES">RULES</el-tab-pane>
+                        <el-tab-pane label="DIPLOMACY">DIPLOMACY</el-tab-pane>
+                    </el-tabs>
+                </el-col>
+            </el-row>
+        </el-main>
+        <el-footer>Footer</el-footer>
+    </el-container>
 </template>
 
 <script type="text/babel">
